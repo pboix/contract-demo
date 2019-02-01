15a4452d34f617da7b0d2a97da232f5723bf85de
Daniel Giralt <danielgiralt@Daniel-Giralts-MB-Pro.local>
2019-02-01 12:30:45 +0100

    chore: move provider initializaiton on beforeAll

--- consumer/consumer.test.js
+++ consumer/consumer.test.js
@@ -3,17 +3,18 @@ const addInteractions = require('./pact_support/addInteractions')
 const Form = require('./form')
 const MOCK_SERVER_PORT = 2202;
 const formId = 'abcdef'
+let provider
 
 describe('Consumer is tested', () => {
-    const provider = new Pact({
-        consumer: "ResultsRenderer",
-        provider: "ResultsAPI",
-        port: MOCK_SERVER_PORT,
-        dir: process.cwd() + '/pacts/',
-        spec: 2
-    });
-
     beforeAll(done => {
+        provider = new Pact({
+            consumer: "ResultsRenderer",
+            provider: "ResultsAPI",
+            port: MOCK_SERVER_PORT,
+            dir: process.cwd() + '/pacts/',
+            spec: 2
+        });
+
         provider.setup()
             .then(() => addInteractions(provider))
             .then(done);
