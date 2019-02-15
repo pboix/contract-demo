cfb26ecc91095807fe4609e63e78d46bcddea516
pau <pau@typeform.com>
2019-02-15 12:33:00 +0100

    Fixed issue with openapi yaml verification

---
+++ swagger/provider/contract_test.go
@@ -0,0 +1,29 @@
+package provider
+
+import (
+	"fmt"
+	"path/filepath"
+	"testing"
+
+	"github.com/pact-foundation/pact-go/dsl"
+	"github.com/pact-foundation/pact-go/types"
+)
+
+func TestProvider(t *testing.T) {
+	pact := &dsl.Pact{
+		Consumer:                 "ResultsRenderer",
+		Provider:                 "ResultsAPI",
+		DisableToolValidityCheck: true,
+	}
+
+	go StartProvider()
+
+	pactDir := "../consumer/pacts"
+	_, err := pact.VerifyProvider(t, types.VerifyRequest{
+		ProviderBaseURL: "http://localhost:8000",
+		PactURLs:        []string{filepath.ToSlash(fmt.Sprintf("%s/resultsrenderer-resultsapi.json", pactDir))},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
--- swagger/provider/provider.go
+++ swagger/provider/provider.go
@@ -11,7 +11,33 @@ func main() {
 
 	mux.HandleFunc("/forms/abcdef/responses", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		fmt.Fprintf(w, fmt.Sprintf(`{"title": "My Awesome Form","form": [{"token": "21085286190ffad1248d17c4135ee56f","answers": [{"field": {"id": "hVONkQcnSNRj","type": "short_text","title": "What's your favourite cheese?"},"type": "text","answer": "Gouda"},{"field": {"id": "RUqkXSeXBXSd","type": "yes_no","title": "do you eat cheese everyday?"},"type": "boolean","answer": true}]}]}`))
+		fmt.Fprintf(w, fmt.Sprintf(`{
+			"title": "My Awesome Form",
+			"form":
+			  {
+				"token": "21085286190ffad1248d17c4135ee56f",
+				"answers": [
+				  {
+					"field": {
+					  "id": "hVONkQcnSNRj",
+					  "type": "short_text",
+					  "title": "What's your favourite cheese?"
+					},
+					"type": "text",
+					"answer": "Gouda"
+				  },
+				  {
+					"field": {
+					  "id": "RUqkXSeXBXSd",
+					  "type": "yes_no",
+					  "title": "do you eat cheese everyday?"
+					},
+					"type": "boolean",
+					"answer": "true"
+				  }
+				]
+			  }	
+		  }`))
 	})
 
 	log.Fatal(http.ListenAndServe(":8000", mux))
