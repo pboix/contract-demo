235998e6d1effbb309b430faea2bb388e930d1ca
Daniel Giralt <danielgiralt@Daniel-Giralts-MB-Pro.local>
2019-02-15 11:19:22 +0100

    chore: prettify response json

--- provider/provider.go
+++ provider/provider.go
@@ -12,7 +12,35 @@ func StartProvider() {
 
 	mux.HandleFunc("/forms/abcdef/responses", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Add("Content-Type", "application/json")
-		fmt.Fprintf(w, fmt.Sprintf(`{"title": "My Awesome Form","form": [{"token": "21085286190ffad1248d17c4135ee56f","answers": [{"field": {"id": "hVONkQcnSNRj","type": "short_text","title": "What's your favourite cheese?"},"type": "text","answer": "Gouda"},{"field": {"id": "RUqkXSeXBXSd","type": "yes_no","title": "do you eat cheese everyday?"},"type": "boolean","answer": true}]}]}`))
+		o := `{
+			"title": "My Awesome Form",
+			"form": [
+				{
+					"token": "21085286190ffad1248d17c4135ee56f",
+					"answers": [
+						{
+							"field": {
+								"id": "hVONkQcnSNRj",
+								"type": "short_text",
+								"title": "What's your favourite cheese?"
+							},
+							"type": "text",
+							"answer": "Gouda"
+						},
+						{
+							"field": {
+								"id": "RUqkXSeXBXSd",
+								"type": "yes_no",
+								"title": "do you eat cheese everyday?"
+							},
+							"type": "boolean",
+							"answer": true
+						}
+					]
+				}
+			]
+		}`
+		fmt.Fprintf(w, fmt.Sprintf(o))
 	})
 
 	log.Fatal(http.ListenAndServe(":8000", mux))
